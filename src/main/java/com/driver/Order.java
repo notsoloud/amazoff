7baa8135cf7d12fa60f8b56ece923e75a969ac3a
Tushar <tushargarg0110@gmail.com>
2022-11-19 03:24:36 +0530

    Deleted test cases

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -8,6 +8,7 @@ public class Order {
     public Order(String id, String deliveryTime) {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
+        //deliveryTime  = HH*60 + MM
     }
 
     public String getId() {
